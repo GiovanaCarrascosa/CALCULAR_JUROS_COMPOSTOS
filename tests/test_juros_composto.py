3e445aaa64f90b9bd12ae44ebbf4a505157ba882
Giovana Carrascosa <giovanaa.carrascosa194@gmail.com>
2025-08-25 12:57:22 -0300

    Feito outras validações de erro e corrigido o de valor string ao invés de int ou float

--- tests/test_juros_composto.py
+++ tests/test_juros_composto.py
@@ -48,7 +48,7 @@ def test_verificar_se_o_valor_da_capital_for_uma_string_ao_inves_de_int_ou_float
     tempo = 2
 
     #executando a funcao e esperando erro
-    with pytest.raises(ValueError, match = "O capital investido deve ser um número (int ou float)."):
+    with pytest.raises(ValueError, match=r"O capital investido deve ser um número \(int ou float\)."):
         calcular_juros_compostos(capital, taxa_juros, tempo)
 
 # caso o valor da taxa nao seja um int ou float
@@ -60,7 +60,7 @@ def test_verificar_se_o_valor_da_taxa_de_juros_for_uma_string_ao_inves_de_int_ou
     tempo = 2
 
     #executando a funcao e esperando erro
-    with pytest.raises(ValueError, match = "A taxa de juros deve ser um número (int ou float)."):
+    with pytest.raises(ValueError, match = r"A taxa de juros deve ser um número \(int ou float\)."):
         calcular_juros_compostos(capital, taxa_juros, tempo)
 
 # caso o valor do tempo nao seja um int ou float
@@ -72,10 +72,78 @@ def test_verificar_se_o_valor_do_tempo_for_uma_string_ao_inves_de_int_ou_float()
     tempo = "ola"
 
     #executando a funcao e esperando erro
-    with pytest.raises(ValueError, match = "O tempo deve ser um número (int ou float)."):
+    with pytest.raises(ValueError, match = r"O tempo deve ser um número \(int ou float\)."):
         calcular_juros_compostos(capital, taxa_juros, tempo)
 
+####################################################################################################################
 
+# caso o valor da capital seja = 0
+def test_verificar_se_o_valor_da_capital_for_igual_a_zero():
 
+    #definindo a entrada
+    capital = 0
+    taxa_juros = 2
+    tempo = 2
 
+    #executando a funcao e esperando erro
+    with pytest.raises(ValueError, match = "O capital investido não pode ser =  0"):
+        calcular_juros_compostos(capital, taxa_juros, tempo)
 
+# caso o valor da taxa de juros seja = 0
+def test_verificar_se_o_valor_da_taxa_juros_for_igual_a_zero():
+
+    #definindo a entrada
+    capital = 1000
+    taxa_juros = 0
+    tempo = 2
+
+    #executando a funcao e esperando erro
+    with pytest.raises(ValueError, match = "A taxa de juros não pode ser =  0"):
+        calcular_juros_compostos(capital, taxa_juros, tempo)
+
+# caso o valor do tempo seja = 0
+def test_verificar_se_o_valor_do_tempo_for_igual_a_zero():
+
+    #definindo a entrada
+    capital = 2000
+    taxa_juros = 2
+    tempo = 0
+
+    #executando a funcao e esperando erro
+    with pytest.raises(ValueError, match = "O tempo não pode ser =  0"):
+        calcular_juros_compostos(capital, taxa_juros, tempo)
+
+####################################################################################################################
+
+# caso o valor da capital nao tenha sido informado
+def test_verificar_se_o_valor_da_capital__nao_foi_preenchida():
+
+    #definindo a entrada
+    taxa_juros = 2
+    tempo = 2
+
+    #executando a funcao e esperando erro
+    with pytest.raises(ValueError, match = "O capital investido não pode ser =  0"):
+        calcular_juros_compostos(taxa_juros, tempo)
+
+# caso o valor da taxa nao tenha sido informado
+def test_verificar_se_o_valor_da_taxa_juros_nao_foi_preenchida():
+
+    #definindo a entrada
+    capital = 20000
+    tempo = 2
+
+    #executando a funcao e esperando erro
+    with pytest.raises(ValueError, match = "O capital investido não pode ser =  0"):
+        calcular_juros_compostos(capital, tempo)
+
+# caso o valor do tempo nao tenha sido informado
+def test_verificar_se_o_valor_do_tempo_nao_foi_preenchido():
+
+    #definindo a entrada
+    capital = 2000
+    taxa_juros = 2
+
+    #executando a funcao e esperando erro
+    with pytest.raises(ValueError, match = "O capital investido não pode ser =  0"):
+        calcular_juros_compostos(capital, taxa_juros)
